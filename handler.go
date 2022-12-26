db8a31bb96bdba1c2ee2f384554be74d77651913
wilvel198 <wvelasquezcorp@gmail.com>
2022-12-26 14:36:31 -0600

    updates

--- handler.go
+++ handler.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 var statusInfo = []StatusInfo{
@@ -35,16 +37,25 @@ func processGetStockInfo() StockInfo {
 	stockDataResults.Days = 20
 
 	serviceData = retrieveStockInfoSvc()
-	processJsonString(serviceData)
+	return processJsonString(serviceData)
 
 	//fmt.Println(serviceData)
-	return stockDataResults
+	//return stockDataResults
 }
 
 // Service to retrieve stock information from data provider
 func retrieveStockInfoSvc() string {
 	var stockInformation string
-	response, err := http.Get("https://www.alphavantage.co/query?apikey=C227WD9W3LUVKVV9&function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM")
+	var apiKey string
+	var stockSymbol string
+
+	os.Setenv("FR_Stock_API_Key", "C227WD9W3LUVKVV9")
+	os.Setenv("FR_Stock_Symbol", "IBM")
+
+	apiKey = os.Getenv("FR_Stock_API_Key")
+	stockSymbol = os.Getenv("FR_Stock_Symbol")
+
+	response, err := http.Get("https://www.alphavantage.co/query?apikey=" + apiKey + "&function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stockSymbol)
 
 	if err != nil {
 		fmt.Print(err.Error())
@@ -63,13 +74,64 @@ func retrieveStockInfoSvc() string {
 
 }
 
-func processJsonString(stockJsonValue string) {
+func processJsonString(stockJsonValue string) StockInfo {
 	fmt.Println("------> Processing the data from service <--------")
+
+	var stockDataResults StockInfo
+	stockDataResults.Name = "IBM"
 
 	byteValue := []byte(stockJsonValue)
 
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result)
 
+	var tempData = result["Time Series (Daily)"]
+	//var symbolInfo = result["Meta Data"]
+
+	fmt.Println("Time Series Daily info ---->")
+	fmt.Println(tempData)
+
+	jsonStr, err := json.Marshal(tempData)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	} else {
+		fmt.Println(string(jsonStr))
+	}
+
+	plan := []byte(jsonStr)
+
+	var data map[string]*stockData
+	json.Unmarshal(plan, &data)
+	//fmt.Printf("Data is %+v\n", data)
+
+	var sum float64 = 0
+	var countItems float64 = 0
+
+	for key, element := range data {
+		countItems = countItems + 1
+
+		var a = element.FourClose
+		stockCloseVal, err := strconv.ParseFloat(a, 32)
+
+		if err != nil {
+			fmt.Println("Error during conversion")
+			return stockDataResults
+		}
+
+		sum = sum + stockCloseVal
+
+		fmt.Println("Key:", key, "=>", "Element:", element.FourClose)
+	}
+
+	stockDataResults.Days = int(countItems)
+	stockDataResults.AvgClosing = roundFloat((sum / countItems), 2)
+	fmt.Println("the sum is " + fmt.Sprintf("%f", sum))
 	fmt.Println("-------> SUCCESS !!!!! <------")
+	return stockDataResults
+
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
--- stockinfo.go
+++ stockinfo.go
@@ -10,3 +10,14 @@ type StatusInfo struct {
 	Status string `json:"status"`
 	Info   string `json:"info"`
 }
+
+type stockData struct {
+	OneOpen               string `json:"1. open"`
+	TwoHigh               string `json:"2. high"`
+	ThreeLow              string `json:"3. low"`
+	FourClose             string `json:"4. close"`
+	FiveAdjustedClose     string `json:"5. adjusted close"`
+	SixVolume             string `json:"6. volume"`
+	SevenDividendAmount   string `json:"7. dividend amount"`
+	EightSplitCoefficient string `json:"8. split coefficient"`
+}
